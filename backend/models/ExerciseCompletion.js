429caf299d6ff0622b3de2ea61be6a4ee3035ad2
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-04-22 19:00:55 +0530

    refactor: update ExerciseCompletion tracking

--- backend/models/ExerciseCompletion.js
+++ backend/models/ExerciseCompletion.js
@@ -4,6 +4,16 @@ const exerciseCompletionSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   exerciseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Exercise', required: true },
   completedAt: { type: Date, default: Date.now },
-}, { timestamps: true });
+}, { 
+  timestamps: true,
+  toJSON: { 
+    virtuals: true,
+    transform: (doc, ret) => {
+      ret.id = ret._id;
+      return ret;
+    }
+  },
+  toObject: { virtuals: true }
+});
 
 module.exports = mongoose.model('ExerciseCompletion', exerciseCompletionSchema);
