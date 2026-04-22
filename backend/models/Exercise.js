a16f4457e8e521cef5840eb700e3eb2d88d692cd
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-04-22 19:00:43 +0530

    refactor: update Exercise model structure

--- backend/models/Exercise.js
+++ backend/models/Exercise.js
@@ -7,6 +7,16 @@ const exerciseSchema = new mongoose.Schema({
   durationMinutes: { type: Number, required: true },
   description: { type: String, required: true },
   steps: [{ type: String }],
-}, { timestamps: true });
+}, { 
+  timestamps: true,
+  toJSON: { 
+    virtuals: true,
+    transform: (doc, ret) => {
+      ret.id = ret._id;
+      return ret;
+    }
+  },
+  toObject: { virtuals: true }
+});
 
 module.exports = mongoose.model('Exercise', exerciseSchema);
