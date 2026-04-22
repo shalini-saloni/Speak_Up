74f5cadf1b68d49c533102f8239448791bc73fe2
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-04-22 19:01:13 +0530

    feat: enhance PracticeSession model

--- backend/models/PracticeSession.js
+++ backend/models/PracticeSession.js
@@ -13,6 +13,16 @@ const practiceSessionSchema = new mongoose.Schema({
     confidenceScore: { type: Number, default: 0 } // 0-100 scale
   },
   aiFeedbackTips: [{ type: String }],
-}, { timestamps: true });
+}, { 
+  timestamps: true,
+  toJSON: { 
+    virtuals: true,
+    transform: (doc, ret) => {
+      ret.id = ret._id;
+      return ret;
+    }
+  },
+  toObject: { virtuals: true }
+});
 
 module.exports = mongoose.model('PracticeSession', practiceSessionSchema);
